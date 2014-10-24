f0f34a5baeeec3e634dcad9034801cc1b7620a5b
Jack Culpepper <jackculpepper@gmail.com>
2014-10-24 14:25:51 -0700

    3 convolutional layers followed by one local layer

---
+++ examples/cfw_local/train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+TOOLS=./build/tools
+
+GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_local/ $TOOLS/caffe train \
+  --solver=examples/cfw_local/cfw_solver.prototxt \
+  --gpu=$1
+
+#  --snapshot=snap/cfw_local/snap_iter_25000.solverstate \
