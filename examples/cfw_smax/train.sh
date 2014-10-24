946a8fe842ab00073b3066897969b1d2bb036aab
Jack Culpepper <jackculpepper@gmail.com>
2014-10-24 14:32:57 -0700

    just one side of the siamese network, for debugging 4 convolutional layers

--- examples/cfw_smax/train.sh
+++ examples/cfw_smax/train.sh
@@ -2,4 +2,6 @@
 
 TOOLS=./build/tools
 
-GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_smax/ $TOOLS/caffe train --solver=examples/cfw_smax/cfw_solver.prototxt --gpu=$1
+GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_smax/ $TOOLS/caffe train \
+  --solver=examples/cfw_smax/cfw_solver.prototxt \
+  --gpu=$1
