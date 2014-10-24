ebf18a53903738457aca13c9689bed075bd5c7fb
Jack Culpepper <jackculpepper@gmail.com>
2014-10-24 14:29:49 -0700

    3 convolutional layers

---
+++ examples/cfw_v3/train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+TOOLS=./build/tools
+
+GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_v3/ $TOOLS/caffe train \
+  --solver=examples/cfw_v3/cfw_solver.prototxt \
+  --gpu=$1
+
+#  --snapshot=snap/cfw_v3/snap_iter_25000.solverstate \
