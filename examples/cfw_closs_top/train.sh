b6f66672673230e9696b4b578dbd850306313f4b
Jack Culpepper <jackculpepper@gmail.com>
2014-10-30 12:55:55 -0700

    train the top part of the net only, starting from a state with two nice kway paths

---
+++ examples/cfw_closs_top/train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+TOOLS=./build/tools
+
+GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_closs_top/ $TOOLS/caffe train \
+  --snapshot=snap/cfw_closs.kway.bottleneck=100/snap_iter_20000.solverstate \
+  --solver=examples/cfw_closs_top/cfw_solver.prototxt \
+  --gpu=$1
+
