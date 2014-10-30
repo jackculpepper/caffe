3a04a41c6be33bdd961f7ea5fba94c1bedd75db4
Jack Culpepper <jackculpepper@gmail.com>
2014-10-30 13:22:05 -0700

    bottleneck 100 works better than 50, so use it

--- examples/cfw_closs_top/train.sh
+++ examples/cfw_closs_top/train.sh
@@ -3,7 +3,7 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_closs_top/ $TOOLS/caffe train \
-  --snapshot=snap/cfw_closs.kway.bottleneck=100/snap_iter_20000.solverstate \
+  --snapshot=snap/cfw_closs.kway/snap_iter_20000.solverstate \
   --solver=examples/cfw_closs_top/cfw_solver.prototxt \
   --gpu=$1
 
