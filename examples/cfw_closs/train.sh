b483241ba5805171ec1273bfdc3d6f8843f975ac
Jack Culpepper <jackculpepper@gmail.com>
2014-10-31 00:28:52 -0700

    clean house does kway + closs on a small training set even converge on that?

--- examples/cfw_closs/train.sh
+++ examples/cfw_closs/train.sh
@@ -3,7 +3,7 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_closs/ $TOOLS/caffe train \
-  --snapshot=snap/cfw_closs.kway/snap_iter_20000.solverstate \
   --solver=examples/cfw_closs/cfw_solver.prototxt \
   --gpu=$1
 
+#  --snapshot=snap/cfw_closs.kway/snap_iter_20000.solverstate \
