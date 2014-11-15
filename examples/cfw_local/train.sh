9c2ad891c02151425c71b23a9721b78ca6f630b6
Jack Culpepper <jackculpepper@gmail.com>
2014-11-14 16:59:01 -0800

    restructuring

--- examples/cfw_local/train.sh
+++ examples/cfw_local/train.sh
@@ -3,7 +3,7 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_local/ $TOOLS/caffe train \
-  --solver=examples/cfw_local/cfw_solver.prototxt \
+  --solver=examples/cfw_local/solver.prototxt \
   --gpu=$1
 
 #  --snapshot=snap/cfw_local/snap_iter_25000.solverstate \
