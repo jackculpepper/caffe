1959681f225c0a7cbc33aadffb127ed513d6fc4b
Jack Culpepper <jackculpepper@gmail.com>
2014-11-14 16:34:22 -0800

    restructured prototxt files

--- examples/cfw_v3/train.sh
+++ examples/cfw_v3/train.sh
@@ -3,7 +3,7 @@
 TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=examples/cfw_v3/ $TOOLS/caffe train \
-  --solver=examples/cfw_v3/cfw_solver.prototxt \
+  --solver=examples/cfw_v3/solver.prototxt \
   --gpu=$1
 
 #  --snapshot=snap/cfw_v3/snap_iter_25000.solverstate \
