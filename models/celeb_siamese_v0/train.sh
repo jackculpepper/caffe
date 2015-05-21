d99551ad86cdd9edc735ebe7e81eeb83f2bd3f92
Jack Culpepper <jackcul@yahoo-inc.com>
2015-05-21 20:12:22 +0000

    resume from a decent spot

--- models/celeb_siamese_v0/train.sh
+++ models/celeb_siamese_v0/train.sh
@@ -4,6 +4,7 @@ TOOLS=./build/tools
 
 GLOG_logtostderr=0 GLOG_log_dir=models/celeb_siamese_v0/ $TOOLS/caffe train \
   --solver=models/celeb_siamese_v0/solver.prototxt \
+  --weights=models/celeb_siamese_v0/celeb_v1_iter_280000.caffemodel \
   --gpu=$1
 
 
